51ee44bfaac155f54b878fe0dc5e5b830b181c58
Vasyl Rokochuk <Vasyl.Rokochuk@iqstudent.com>
2020-02-25 17:07:29 +0000

    Init

---
+++ ReflectIt/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReflectIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeList = CreateCollection(typeof(List<>), typeof(Employee));
+            Console.WriteLine(employeeList.GetType().Name);
+            var geneicArgs = employeeList.GetType().GenericTypeArguments;
+
+            foreach(var arg in geneicArgs)
+            {
+                Console.Write("[{0}]", arg.Name);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static object CreateCollection(Type collectionType, Type itemType)
+        {
+            return Activator.CreateInstance(collectionType);
+        }
+
+        public class Employee
+        {
+            public string Name { get; set; }
+        }
+    }
+}
